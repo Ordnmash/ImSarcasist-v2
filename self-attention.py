7c6e28fcf5138d3c423a7f7efe2d59519ebd81dd
Ordnmash <ordnfxist@gmail.com>
2026-08-24 20:25:06 +0200

    Rename variables for clarity in self-attention.py

--- self-attention.py
+++ self-attention.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
 
-q = nn.Linear(20, 10, bias=False)
-k = nn.Linear(20, 10, bias=False)
-v = nn.Linear(20, 10, bias=False)
+query = nn.Linear(20, 10, bias=False)
+key   = nn.Linear(20, 10, bias=False)
+value = nn.Linear(20, 10, bias=False)
