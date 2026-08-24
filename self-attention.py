225723dc26352d1afaa7a5c78d5cda87dafbf347
Ordnmash <ordnfxist@gmail.com>
2026-08-24 20:27:51 +0200

    Add inputV tensor initialization in self-attention.py

--- self-attention.py
+++ self-attention.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 n_embd = 10
 block_size = 10
 
+inputV = torch.randn((block_size, n_embd),)
+
 query = nn.Linear(n_embd, n_embd, bias=False)
 key   = nn.Linear(n_embd, n_embd, bias=False)
 value = nn.Linear(n_embd, n_embd, bias=False)
