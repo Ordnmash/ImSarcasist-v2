86c5e85e1c89cebc7a31b757b61bef7d7c486d14
Ordnmash <ordnfxist@gmail.com>
2026-08-24 20:28:45 +0200

    Specify dtype for input tensor in self-attention.py

--- self-attention.py
+++ self-attention.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 n_embd = 10
 block_size = 10
 
-inputV = torch.randn((block_size, n_embd),)
+inputV = torch.randn((block_size, n_embd), dtype=torch.float32)
 
 query = nn.Linear(n_embd, n_embd, bias=False)
 key   = nn.Linear(n_embd, n_embd, bias=False)
