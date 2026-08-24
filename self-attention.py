c17ba0b15f0525e099ab1be136097e501cee17a5
Ordnmash <ordnfxist@gmail.com>
2026-08-24 20:30:06 +0200

    Add query, key, and value computations in self-attention

--- self-attention.py
+++ self-attention.py
@@ -9,3 +9,7 @@ inputV = torch.randn((block_size, n_embd), dtype=torch.float32)
 query = nn.Linear(n_embd, n_embd, bias=False)
 key   = nn.Linear(n_embd, n_embd, bias=False)
 value = nn.Linear(n_embd, n_embd, bias=False)
+
+q = query(inputV)
+k = key(inputV)
+v = value(inputV)
