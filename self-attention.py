cf1c3e47e5bb94671d33a5602e34d2252e78977d
Ordnmash <ordnfxist@gmail.com>
2026-08-24 20:26:51 +0200

    Refactor Linear layers for self-attention

--- self-attention.py
+++ self-attention.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 
-query = nn.Linear(20, 10, bias=False)
-key   = nn.Linear(20, 10, bias=False)
-value = nn.Linear(20, 10, bias=False)
+n_embd = 10
+block_size = 10
+
+query = nn.Linear(n_embd, n_embd, bias=False)
+key   = nn.Linear(n_embd, n_embd, bias=False)
+value = nn.Linear(n_embd, n_embd, bias=False)
