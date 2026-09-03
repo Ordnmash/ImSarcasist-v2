228365e64b521372b706831459bb93e2a19e0091
ordnmash <ordnfxist@gmail.com>
2026-09-03 21:43:52 +0200

    Refactor self-attention.py by removing setup code
    Removed initial setup for self-attention computation.

--- self-attention.py
+++ self-attention.py
@@ -2,19 +2,4 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-n_embd = 10
-block_size = 15
-
-inputV = torch.randn((block_size, n_embd), dtype=torch.float32)
-
-query = nn.Linear(n_embd, n_embd, bias=False)
-key   = nn.Linear(n_embd, n_embd, bias=False)
-value = nn.Linear(n_embd, n_embd, bias=False)
-
-q = query(inputV)
-k = key(inputV)
-v = value(inputV)
-
-wei = q @ k.T # add masked fill
-wei = wei / torch.sqrt(torch.tensor(k.shape[0])) # normalizing before softmax
-attention = F.softmax(wei, dim=0) @ v            # note there's no masked fill in this example
+# let's build SelfAttention block from scratch
