8ae977a62371a487424d8cc35ff5ae971493f55e
ordnmash <ordnfxist@gmail.com>
2026-09-03 21:45:37 +0200

    Implement SelfAttention class
    Added SelfAttention class to implement self-attention mechanism.

--- self-attention.py
+++ self-attention.py
@@ -3,3 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # let's build SelfAttention block from scratch
+class SelfAttention(nn.Module):
+  def __init__(self, insize, outsize):
+    super().__init__()
+    # attributes of SelfAttention follows
